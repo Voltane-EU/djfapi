008e5bcd516c3ca4bdd58d4fa3dac53c1796abd9
Manuel Stingl <manuel@stingl.st>
2023-11-04 16:00:29 +0100

    handle user with no permissions

--- djfapi/redis/models.py
+++ djfapi/redis/models.py
@@ -7,15 +7,19 @@ class AbstractUserTransactionTokenDjangoPermissions2RedisMixin:
     redis = None
 
     def _save_transaction_token(self, token_id: str):
+        permissions = [
+            JWTTokenDjangoPermissions.convert_permission_to_scope(permission)
+            for permission in self.get_all_permissions()
+        ]
+        if not permissions:
+            return
+
         if not self.redis:
             self.redis = get_connection(self.redis_url)
 
         key = f'access:token:{token_id}:scopes'
         self.redis.sadd(
             key,
-            *[
-                JWTTokenDjangoPermissions.convert_permission_to_scope(permission)
-                for permission in self.get_all_permissions()
-            ],
+            *permissions,
         )
         self.redis.expire(key, 310)  # Transaction Token expires after 5 minutes (300s) + buffer (10s)
