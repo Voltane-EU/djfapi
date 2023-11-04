d0bfb461b465bc805ce27a0792df69b8eac794b0
Manuel Stingl <manuel@stingl.st>
2023-11-04 16:10:16 +0100

    fix regex

--- djfapi/security/jwt.py
+++ djfapi/security/jwt.py
@@ -113,7 +113,9 @@ class JWTToken(APIKeyHeader):
 
 
 class JWTTokenDjangoPermissions(JWTToken):
-    DJANGO_PERMISSION_REGEX = r"^(?P<service>[_\w]+)\.(?P<action>\w+)_(?P<resource>\w+)(_(?P<selector>.*))?$"
+    DJANGO_PERMISSION_REGEX = (
+        r"^(?P<service>[_\w]+)\.(?P<action>[^a-zA-Z0-9]+)_(?P<resource>[^a-zA-Z0-9]+)(_(?P<selector>.*))?$"
+    )
     DJANGO_PERMISSION_ACTION_TO_CRUD = {
         'add': 'create',
         'view': 'read',
